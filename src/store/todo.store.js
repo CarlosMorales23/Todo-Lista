8f89875256e679a4445098f93dc56b81f32f2435
Carlos Morales <110312174+CarlosMorales23@users.noreply.github.com>
2023-09-19 09:19:45 -0300

    se implementan los metodos

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -1,6 +1,6 @@
 import { Todo } from "../todos/models/todo.model";
 
-const Filter = {
+const Filters = {
     All: 'all',
     Completed: 'Completed',
     Pending: 'Pending'
@@ -33,13 +33,32 @@ const loadStore = () => {
 
 }
 
+
+const getTodos = (filter= Filters.All ) => {
+
+    switch ( filter ) {
+        case Filters.All:
+            return[...state.todos];
+        
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done )
+        
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done)
+
+        default:
+            throw new Error(`Option ${ filter } is not valid`);
+    }
+}
+
 /**
  * Agrega tareas a la lista.
  * @param {String} description 
  */
 const addTodo = ( description ) => {
-    throw new Error('Not implemented')
+    if (!description) throw new Error('description is required')
 
+    state.todos.push(new Todo(description))
 }
 
 /**
@@ -55,24 +74,30 @@ const toggleTodo = ( todoId ) => {
  * @param {String} todoId todo identifier
  */
 const deleteTodo = ( todoId ) => {
-    throw new Error('Not implemented')
+
+    state.todos = state.todos.filter( todo => todo.id !== todoId);
 }
 
 /**
  * Elimina todas las tareas completadas
  * @param {} todoId 
  */
-const deleteCompleted = ( todoId ) => {
-    throw new Error('Not implemented')
+const deleteCompleted = () => {
+    state.todos = state.todos.filter( todo => todo.done);
 }
 
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFilter = ( newFilter = Filters.All ) => {
+    //* se puede validad con object.keys(filters) */
 
-const setFilter = ( newFilter = Filter.All ) => {
-    throw new Error('Not implemented')
+    state.filter = newFilter;
 }
 
 const getCurrentFilter = (  ) => {
-    throw new Error('Not implemented')
+    return state.filter;
 }
 
 
@@ -81,6 +106,7 @@ export default {
     deleteCompleted,
     deleteTodo,
     getCurrentFilter,
+    getTodos,
     initStore,
     loadStore,
     setFilter,
