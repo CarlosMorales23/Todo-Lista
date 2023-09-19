74c07f34b94b13e3b7e7e145076c2e46afddd261
Carlos Morales <110312174+CarlosMorales23@users.noreply.github.com>
2023-09-19 06:09:38 -0300

    implementación de identificador unico

--- src/todos/models/todo.model.js
+++ src/todos/models/todo.model.js
@@ -1,4 +1,7 @@
-
+import { v4 as uuid } from "uuid";
+/*uuid es un paquete de identificador unico que brinda npm,  
+  acá estoy usando la versión 4. 
+*/
 
 export class Todo {
 
@@ -7,7 +10,7 @@ export class Todo {
      * @param {String} description 
      */
     constructor (description){
-        this.id= 1;
+        this.id= uuid();
         this.description = description;
         this.done = false;
         this.createdAt = new Date();
