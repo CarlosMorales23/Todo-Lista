79d0d84129a5976a6fdda161c4a8079f8a29cecf
Carlos Morales <110312174+CarlosMorales23@users.noreply.github.com>
2023-09-28 07:17:43 -0300

    se implementa displayTodos función para renderizar los todos

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,4 +1,5 @@
 //import { html } from "./app.html";  asi da error, no lo reconoce
+import todoStore from "../store/todo.store";
 import  html from "./app.html?raw"; //de esta forma lo reconoce en crudo, y no da error
 //TODO cuando va entre comillas {html}?
 
@@ -9,6 +10,9 @@ import  html from "./app.html?raw"; //de esta forma lo reconoce en crudo, y no d
  */
 export const App = (elementId) => {
 
+    const displayTodos = () => {
+        const todos = todoStore.getTodos( todoStore.getCurrentFilter() )
+    }
 
     //Función auto-invocada
     //Se monta cuando la función App se llama
@@ -16,6 +20,7 @@ export const App = (elementId) => {
         const app = document.createElement('div');
         app.innerHTML = html; //codigo html importado desde app.html
         document.querySelector(elementId).append(app);
+        displayTodos();
     })();
 
 }
